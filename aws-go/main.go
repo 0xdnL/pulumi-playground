1471ab3234c1c95d8a3c4d672a1b69623d4d0fb5
Daniel Tinney <dnltinney@gmail.com>
2020-12-07 13:28:40 +0100

    add serving static s3-content

--- aws-go/main.go
+++ aws-go/main.go
@@ -10,7 +10,11 @@ func main() {
 	pulumi.Run(func(ctx *pulumi.Context) error {
 
 		// Create an AWS resource (S3 Bucket)
-		bucket, err := s3.NewBucket(ctx, "puliumi-playground", nil)
+		bucket, err := s3.NewBucket(ctx, "puliumi-playground", &s3.BucketArgs{
+			Website: s3.BucketWebsiteArgs{
+				IndexDocument: pulumi.String("index.html"),
+			},
+		})
 		if err != nil {
 			return err
 		}
@@ -22,8 +26,10 @@ func main() {
 
 		// providing bucket created earlier as an input to new BucketObject
 		_, err = s3.NewBucketObject(ctx, "index.html", &s3.BucketObjectArgs{
-			Bucket: bucket.ID(),
-			Content: pulumi.String(string(htmlContent)),
+			Acl:         pulumi.String("public-read"),
+			ContentType: pulumi.String("text/html"),
+			Bucket:      bucket.ID(),
+			Content:     pulumi.String(string(htmlContent)),
 		})
 		if err != nil {
 			return err
@@ -31,6 +37,7 @@ func main() {
 
 		// Export the name of the bucket
 		ctx.Export("bucketName", bucket.ID())
+		ctx.Export("bucketEndpoint", pulumi.Sprintf("http://%s", bucket.WebsiteEndpoint))
 		return nil
 
 	})
